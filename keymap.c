3354322f34f2b659536c8f6d94457093089fecaf
Carnavale <adran@hey.com>
2025-10-29 18:09:09 -0300

    Refactor keymap modifiers and layers
    Updated home row modifier definitions and added transparent layer mods.

--- keymap.c
+++ keymap.c
@@ -24,64 +24,92 @@
 
 #define UNICODE_SELECTED_MODES UC_LNX
 
-// Home row mod aliases (max 5 letters)
-#define GA LGUI_T(KC_A)
-#define AA LALT_T(KC_S)
-#define CA LCTL_T(KC_D)
-#define SA LSFT_T(KC_F)
-#define SJ RSFT_T(KC_J)
-#define CK RCTL_T(KC_K)
-#define AL RALT_T(KC_L)
-#define GQ RGUI_T(KC_QUOT)
+// thumb key mods
 
-// Additional home row mods for X and .
-#define XA RALT_T(KC_X)
-#define DA RALT_T(KC_DOT)
-
-// Mod taps for layers
-#define SJ1 RSFT_T(KC_TRANSPARENT)
-#define CK1 RCTL_T(KC_TRANSPARENT)
-#define AL1 RALT_T(KC_TRANSPARENT)
-#define GQ1 RGUI_T(KC_TRANSPARENT)
-
-#define GA2 LGUI_T(KC_TRANSPARENT)
-#define AA2 LALT_T(KC_TRANSPARENT)
-#define CA2 LCTL_T(KC_TRANSPARENT)
-#define SA2 LSFT_T(KC_TRANSPARENT)
-
-#define SJ3 RSFT_T(KC_TRANSPARENT)
-#define CK3 RCTL_T(KC_TRANSPARENT)
-#define AL3 RALT_T(KC_TRANSPARENT)
-#define GQ3 RGUI_T(KC_TRANSPARENT)
-
-#define GA4 LGUI_T(KC_SCLN)
-#define AA4 LALT_T(KC_4)
-#define CA4 LCTL_T(KC_5)
-#define SA4 LSFT_T(KC_6)
-
-// Additional home row mods for layer 2 right
-#define SJ2 RSFT_T(KC_LEFT)
-#define CK2 RCTL_T(KC_DOWN)
-#define AL2 RALT_T(KC_UP)
-#define GQ2 RGUI_T(KC_RIGHT)
-
-// Additional home row mods for layer 3 left
-#define GA3 LGUI_T(KC_COLN)
-#define AA3 LALT_T(KC_DLR)
-#define CA3 LCTL_T(KC_PERC)
-#define SA3 LSFT_T(KC_CIRC)
-
-// Additional home row mods for layer 4 right
-#define SJ4 RSFT_T(KC_TRANSPARENT)
-#define CK4 RCTL_T(KC_TRANSPARENT)
-#define AL4 RALT_T(KC_TRANSPARENT)
-#define GQ4 RGUI_T(KC_TRANSPARENT)
-
-// Thumb key aliases
 #define TAB1 LT(1, KC_TAB)
 #define ENT2 LT(2, KC_ENT)
 #define SPC3 LT(3, KC_SPC)
 #define BSPC4 LT(4, KC_BSPC)
+
+// layer 0 mods
+
+// left
+#define GA LGUI_T(KC_A)
+#define AA LALT_T(KC_S)
+#define CA LCTL_T(KC_D)
+#define SA LSFT_T(KC_F)
+#define XA RALT_T(KC_X)
+
+// right
+#define SJ RSFT_T(KC_J)
+#define CK RCTL_T(KC_K)
+#define AL RALT_T(KC_L)
+#define GQ RGUI_T(KC_QUOT)
+#define DA RALT_T(KC_DOT)
+
+// layer 1 mods
+
+// left
+// #define GA1 LGUI_T(KC_F11)
+// #define AA1 LALT_T(KC_F4)
+// #define CA1 LCTL_T(KC_F5)
+// #define SA1 LSFT_T(KC_F6)
+// #define XA1 RALT_T(KC_F1)
+
+// right
+#define SJ1 RSFT_T(KC_TRANSPARENT)
+#define CK1 RCTL_T(KC_TRANSPARENT)
+#define AL1 RALT_T(KC_TRANSPARENT)
+#define GQ1 RGUI_T(KC_TRANSPARENT)
+#define DA1 RALT_T(KC_TRANSPARENT)
+
+// layer 2 mods
+
+// left
+#define GA2 LGUI_T(KC_TRANSPARENT)
+#define AA2 LALT_T(KC_TRANSPARENT)
+#define CA2 LCTL_T(KC_TRANSPARENT)
+#define SA2 LSFT_T(KC_TRANSPARENT)
+#define XA2 RALT_T(KC_TRANSPARENT)
+
+// right
+// #define SJ2 RSFT_T(KC_DOWN)
+// #define CK2 RCTL_T(KC_UP)
+// #define AL2 RALT_T(KC_RIGHT)
+// #define GQ2 RGUI_T(KC_TRANSPARENT)
+// #define DA2 RALT_T(KC_TRANSPARENT)
+
+// layer 3 mods
+
+// left
+// #define GA3 LGUI_T(KC_COLN)
+// #define AA3 LALT_T(KC_DLR)
+// #define CA3 LCTL_T(KC_PERC)
+// #define SA3 LSFT_T(KC_CIRC)
+// #define XA3 RALT_T(KC_EXLM)
+
+//right
+#define SJ3 RSFT_T(KC_TRANSPARENT)
+#define CK3 RCTL_T(KC_TRANSPARENT)
+#define AL3 RALT_T(KC_TRANSPARENT)
+#define GQ3 RGUI_T(KC_TRANSPARENT)
+#define DA3 RALT_T(KC_TRANSPARENT)
+
+// layer 4 mods
+
+//left
+// #define GA4 LGUI_T(KC_SCLN)
+// #define AA4 LALT_T(KC_4)
+// #define CA4 LCTL_T(KC_5)
+// #define SA4 LSFT_T(KC_6)
+// #define XA4 RALT_T(KC_1)
+
+// right
+#define SJ4 RSFT_T(KC_TRANSPARENT)
+#define CK4 RCTL_T(KC_TRANSPARENT)
+#define AL4 RALT_T(KC_TRANSPARENT)
+#define GQ4 RGUI_T(KC_TRANSPARENT)
+#define DA4 RALT_T(KC_TRANSPARENT)
 
 enum combo_events {
     COMBO_ESC,
@@ -103,52 +131,52 @@ combo_t key_combos[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
-        KC_NO, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,  KC_O,    KC_P,   KC_NO,
+    KC_NO, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_NO,
 
-        KC_NO, GA,      AA,      CA,      SA,      KC_G,         KC_H,    SJ,      CK,    AL,      GQ,     KC_NO, KC_NO,
+    KC_NO, GA,      AA,      CA,      SA,      KC_G,           KC_H,    SJ,      CK,      AL,       GQ,      KC_NO,
 
-        KC_Z,  XA,      KC_C,    KC_V,    KC_B,    KC_N,         KC_M,    KC_COMM, DA,    KC_SLSH, KC_NO,
+    KC_NO, KC_Z,    XA,      KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, DA,       KC_SLSH, KC_NO,
 
-                                 KC_NO,   TAB1,    ENT2,         SPC3,    BSPC4,   KC_NO
+                             KC_NO,   TAB1,    ENT2,           SPC3,    BSPC4,   KC_NO
     ),
 
     [1] = LAYOUT_split_3x6_3(
-        KC_NO, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_NO,        KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
 
-        KC_NO, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_NO,        KC_NO,   SJ1,     CK1,   AL1,     GQ1,    KC_NO,
+    KC_NO, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_NO,          KC_NO,   SJ1,     CK1,     AL1,      GQ1,     KC_NO,
 
-        KC_NO, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_NO,        KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_NO,          KC_NO,   KC_NO,   KC_NO,   DA1,      KC_NO,   KC_NO,
 
-                                 KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO
+                             KC_NO,   KC_NO,   KC_NO,          KC_NO,   KC_NO,   KC_NO
     ),
 
     [2] = LAYOUT_split_3x6_3(
-        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_PSCR, KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,   KC_PSCR, KC_NO,   KC_NO,    KC_NO,   KC_NO,
 
-        KC_NO, GA2,     AA2,     CA2,     SA2,     KC_NO,        SJ2,     CK2,     AL2,   GQ2,     KC_NO,  KC_NO,
+    KC_NO, GA2,     AA2,     CA2,     SA2,     KC_NO,          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,   KC_NO,
 
-        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_HOME, KC_NO,   KC_NO, KC_NO,   KC_END, KC_NO,
+    KC_NO, KC_NO,   XA2,     KC_NO,   KC_NO,   KC_NO,          KC_HOME, KC_NO,   KC_NO,   KC_NO,    KC_END,  KC_NO,
 
-                                 KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO
+                             KC_NO,   KC_NO,   KC_NO,          KC_NO,   KC_NO,   KC_NO
     ),
 
     [3] = LAYOUT_split_3x6_3(
-        KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,      KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,        KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
 
-        KC_NO, GA3,     AA3,     CA3,     SA3,     KC_PLUS,      KC_NO,   SJ3,     CK3,   AL3,     GQ3,    KC_NO,
+    KC_NO, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,        KC_NO,   SJ3,     CK3,     AL3,      GQ3,     KC_NO,
 
-        KC_NO, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,      KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,        KC_NO,   KC_NO,   KC_NO,   DA3,      KC_NO,   KC_NO,
 
-                                 KC_NO,   KC_RPRN, KC_UNDS,      KC_NO,   KC_NO,   KC_NO
+                             KC_NO,   KC_RPRN, KC_UNDS,        KC_NO,   KC_NO,   KC_NO
     ),
 
     [4] = LAYOUT_split_3x6_3(
-        KC_NO, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,      KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,        KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
 
-        KC_NO, GA4,     AA4,     CA4,     SA4,     KC_EQL,       SJ4,     CK4,     AL4,   GQ4,     KC_NO,  KC_NO,
+    KC_NO, KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,         SJ4,     CK4,     AL4,     GQ4,      KC_NO,   KC_NO,
 
-        KC_NO, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,      KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+    KC_NO, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,        KC_NO,   KC_NO,   KC_NO,   DA4,      KC_NO,   KC_NO,
 
-                                 KC_NO,   KC_0,    KC_MINS,      KC_NO,   KC_NO,   KC_NO
+                             KC_NO,   KC_0,    KC_MINS,        KC_NO,   KC_NO,   KC_NO
     )
 };
